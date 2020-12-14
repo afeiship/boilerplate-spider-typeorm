2fac8141521b58fd99d9da37cc7268baa7814a89
afeiship <1290657123@qq.com>
2020-12-14 14:14:07 +0800

    feat: add setup yml

--- src/app/post.ts
+++ src/app/post.ts
@@ -2,12 +2,15 @@ import "reflect-metadata";
 import Abstract from './abstract';
 import Post from '../entities/post';
 
+const { conf } = nx.$global;
+
+
 class App extends Abstract {
   public async start() {
     const manager = await this.connect();
     const posts = await manager.find(Post, { where: { grabbed: false } });
-    console.log(typeof nx);
     console.log('posts:', posts, nx.VERSION);
+    console.log(conf.gets());
   }
 }
 
--- src/app/setup.ts
+++ src/app/setup.ts
@@ -1,1 +1,12 @@
-import '@jswork/next';
+import '@jswork/next-require';
+
+nx.require({
+  pattern: ['@jswork/next-*', '@afeiship/next-*', '!@jswork/next-require'],
+  scope: ['dependencies']
+});
+
+nx.global({
+  conf: new nx.YamlConfiguration({ path: './config.yml' }),
+  toJson: nx.toAction('json'),
+  toHtml: nx.toAction('text')
+});
